901aaf2dfa1d7e3083717b438d12faaa7af6581d
Humberto Arana <humaranah@outlook.com>
2022-06-07 23:29:47 -0500

    Add dependency injection

--- src/Panther.POC/App.xaml.cs
+++ src/Panther.POC/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using NAudio.Wave;
+using Panther.Core.Services;
 using System.Windows;
 
 namespace Panther.WPF;
@@ -7,4 +9,24 @@ namespace Panther.WPF;
 /// </summary>
 public partial class App : Application
 {
+    private readonly ServiceProvider _serviceProvider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    private static void ConfigureServices(IServiceCollection services) =>
+        services
+            .AddSingleton<WaveOutEvent>()
+            .AddSingleton<IMusicPlayer, MusicPlayer>()
+            .AddSingleton<MainWindow>();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainWindow = _serviceProvider.GetService<MainWindow>();
+        mainWindow?.Show();
+    }
 }
