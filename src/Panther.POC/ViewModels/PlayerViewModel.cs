1846580c074897dbd33242580f6bcc5eb5110872
Humberto Arana <humaranah@outlook.com>
2022-06-07 23:18:27 -0500

    Rename WPF project into POC

---
+++ src/Panther.POC/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Panther.POC.ViewModels;
+
+public class PlayerViewModel
+{
+    public string SongName { get; private set; }
+    public string AlbumName { get; private set; }
+    public string ComposerName { get; private set; }
+}
