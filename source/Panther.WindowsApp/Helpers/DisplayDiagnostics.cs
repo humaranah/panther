9014fb5e672fe63022e75eb3fadb08f6218ac99e
Humberto Arana <humaranah@outlook.com>
2025-09-13 10:27:33 -0500

    Set minimum window size

---
+++ source/Panther.WindowsApp/Helpers/DisplayDiagnostics.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using System.Runtime.InteropServices;
+using WinRT.Interop;
+
+namespace Panther.WindowsApp.Helpers;
+
+public static partial class DisplayDiagnostics
+{
+    [LibraryImport("user32.dll")]
+    private static partial uint GetDpiForWindow(nint hWnd);
+
+    public static double GetDpi(Window window)
+    {
+        var hwnd = WindowNative.GetWindowHandle(window);
+        return GetDpiForWindow(hwnd);
+    }
+}
--- source/Panther.WindowsApp/MainWindow.xaml.cs
+++ source/Panther.WindowsApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Panther.WindowsApp.Helpers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -11,10 +12,26 @@ namespace Panther.WindowsApp;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private const double DefaultDpi = 96.0;
+
     public MainWindow()
     {
         InitializeComponent();
+
         ExtendsContentIntoTitleBar = true;
         AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Standard;
+
+        SetMinimumWindowSize(320, 380);
+    }
+
+    private void SetMinimumWindowSize(int width, int height)
+    {
+        var dpi = DisplayDiagnostics.GetDpi(this);
+        var scale = dpi / DefaultDpi;
+
+        OverlappedPresenter presenter = OverlappedPresenter.Create();
+        presenter.PreferredMinimumWidth = (int)(width * scale);
+        presenter.PreferredMinimumHeight = (int)(height * scale);
+        AppWindow.SetPresenter(presenter);
     }
 }
